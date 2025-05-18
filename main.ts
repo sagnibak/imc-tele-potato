d20a5da8d46500d5d4525328734b565b22c033c2
Sagnik Bhattacharya <sagnikdps@gmail.com>
2025-05-18 16:59:09 -0500

    Remove Debugging Code

--- main.ts
+++ main.ts
@@ -1,33 +1,3 @@
-//  potato = 0
-//  def on_received_number(receivedNumber):
-//      global potato
-//      potato = receivedNumber
-//  radio.on_received_number(on_received_number)
-//  def on_gesture_shake():
-//      global potato
-//      if potato > 0:
-//          radio.send_number(potato)
-//          potato = -1
-//  input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-//  def on_button_pressed_ab():
-//      global potato
-//      potato = randint(10, 20)
-//  input.on_button_pressed(Button.AB, on_button_pressed_ab)
-//  radio.set_group(1)
-//  potato = -1
-//  def on_forever():
-//      global potato
-//      if potato == 0:
-//          basic.show_icon(IconNames.SKULL)
-//      if potato < 0:
-//          basic.clear_screen()
-//      if potato > 0:
-//          basic.show_icon(IconNames.CHESSBOARD)
-//          potato += -1
-//  basic.forever(on_forever)
-// #########################
-// #########################
-// #########################
 let POTATO_CLEAR = -2
 let POTATO_INIT = -1
 let POTATO_DEAD = 0
