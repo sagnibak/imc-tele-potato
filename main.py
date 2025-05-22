9c46e10957154ca34ee002ac2c1da9e108117f0c
Sagnik Bhattacharya <sagnikdps@gmail.com>
2025-05-22 09:21:27 -0500

    Play Music on Death

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ POTATO_CLEAR = -2
 POTATO_INIT = -1
 POTATO_DEAD = 0
 
+PLAYED_DEATH_MUSIC = False
+
 def on_button_pressed_ab():
     global potato
     potato = randint(10, 20)
     led.stop_animation()
+    PLAYED_DEATH_MUSIC = False
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_gesture_shake():
@@ -27,8 +30,14 @@ def on_forever():
     global potato
     if potato == POTATO_DEAD:
         basic.show_icon(IconNames.SKULL)
+        if not PLAYED_DEATH_MUSIC:
+            music.play(
+                music.builtin_playable_sound_effect(soundExpression.sad),
+                music.PlaybackMode.IN_BACKGROUND
+            )
     if potato == POTATO_INIT:
         basic.show_string("Press A+B")
+        PLAYED_DEATH_MUSIC = False
     if potato <= POTATO_CLEAR:
         basic.clear_screen()
     if potato > 0:
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 let POTATO_CLEAR = -2
 let POTATO_INIT = -1
 let POTATO_DEAD = 0
+let PLAYED_DEATH_MUSIC = false
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     
     potato = randint(10, 20)
     led.stopAnimation()
+    let PLAYED_DEATH_MUSIC = false
 })
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     
@@ -21,13 +23,19 @@ radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
 let potato = POTATO_INIT
 radio.setGroup(1)
 basic.forever(function on_forever() {
+    let PLAYED_DEATH_MUSIC: boolean;
     
     if (potato == POTATO_DEAD) {
         basic.showIcon(IconNames.Skull)
+        if (!PLAYED_DEATH_MUSIC) {
+            music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.InBackground)
+        }
+        
     }
     
     if (potato == POTATO_INIT) {
         basic.showString("Press A+B")
+        PLAYED_DEATH_MUSIC = false
     }
     
     if (potato <= POTATO_CLEAR) {
